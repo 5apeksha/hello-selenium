8aceb57daf3b437dafd1273e2f9eb58c41ef4aa0
panache-chinmay <chinmaydeshpande010@gmail.com>
2018-06-03 21:10:33 +0530

    Error

---
+++ myScript.js
@@ -0,0 +1,58 @@
+// alert("Hello Nitesh");
+// console.log("Hello India ");
+// document.write("<h1> Hello world</h1>");
+// alert("Thanks for Coming ! Visit Again");
+//
+// // ----------------->
+// var message ;
+// message ="hello world";
+// alert(message);
+// message = "hello to js world"
+// alert(message);
+
+// varuiable name cannot start with number
+// $ and _ special characters are alllowes
+// var message = "Hello!"
+// alert(message);
+// message = "She's great"
+// alert(message);
+// message = 'she is \"super\" ';
+// alert(message);
+// message = 'she is \'super\' ';
+// alert(message);
+
+
+// var name =prompt("What is your name ?");
+// console.log(name);
+// alert("Hello " + name );
+//
+// var hello = "hello";
+// var num = 12  ;
+// var num1 = 13 ;
+// var num12 = "12";
+// console.log(hello + num );
+// console.log(num + num1 );
+// console.log(intnum12 + num );
+
+
+// var visitorname = prompt("what is your name! ");
+// var message = "welcome to treehouse  " + visitorname + " ";
+// message += "learn javascript ";
+// message += "visit again";
+// document.write(message);
+
+
+// var ny = "chinmay";
+// console.log(ny.length);
+// console.log(ny.toUpperCase());
+// console.log(ny.toLowerCase());
+
+
+
+var adjective = prompt("Enter the adjective");
+var verb = prompt("Enter the verb");
+var lang = prompt("Enter the lang");
+alert("you are done");
+var message ="Chinmay is " + adjective;
+message += " "+ verb + " language to " + lang;
+document.write(message);
